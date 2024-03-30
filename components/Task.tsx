55a51dd6a3895379c9b019531ca28a6b20328e6d
Abusantos <abusomwansantos@gmail.com>
2024-03-30 03:43:22 +0100

    updating the task

--- components/Task.tsx
+++ components/Task.tsx
@@ -7,22 +7,37 @@ import { useState } from "react";
 const Task = ({ }) => {
     // const { data: session } = useSession()
     const [activeTab, setActiveTab] = useState("new");
+    const handleTabChange = (tab: string) => {
+        setActiveTab(tab)
+    }
     return (
         <section className="w-full flex flex-col items-center justify-center">
-            <div className="flex space-x-2 w-11/12 items-center text-[0.8rem] ">
+            <div className="flex space-x-2 w-full items-center text-[0.8rem]">
                 <button
-                    onClick={(prev) => setActive(true)}
-                    className="border-2 border-[#6229b3] bg-[#dfd7fb] text-[#6229b3] font-medium  rounded-xl pb-2 pl-3 pr-3 pt-2">New Tasks</button>
+                    onClick={() => handleTabChange("new")}
+                    className={`border-2 bg-[#dfd7fb] ml-2 font-medium rounded-xl py-2 px-4 ${activeTab === "new" ? "border-[#6229b3] text-[#6229b3]" : "border-[#6329b346] text-[#6329b362]"
+                        }`}>New Tasks</button>
                 <button
-                    className="border-2 border-[#6329b346] bg-[#dfd7fb] text-[#6329b362] font-medium  rounded-xl p-2 ">Ongoing Task</button>
-                <button className="border-2 border-[#6329b346] bg-[#dfd7fb] 
-                text-[#6329b362] font-medium  rounded-xl p-2">Completed Tasks</button>
+                    onClick={() => handleTabChange("ongoing")}
+                    className={`border-2 bg-[#dfd7fb] font-medium rounded-xl  py-2 px-4 ${activeTab === "ongoing" ? "border-[#6229b3] text-[#6229b3]" : "border-[#6329b346] text-[#6329b362]"
+                        }`}>Ongoing Task</button>
+                <button
+                    onClick={() => handleTabChange("completed")}
+                    className={`border-2 bg-[#dfd7fb] font-medium rounded-xl py-2 px-4 ${activeTab === "completed" ? "border-[#6229b3] text-[#6229b3]" : "border-[#6329b346] text-[#6329b362]"
+                        }`}>Completed Tasks</button>
             </div>
 
-            {/* <p className="desc text-left">{desc}</p> */}
             <div className="mt-10  prompt_layout ">
                 {
-                    active ? <p> New Task</p> : <p>ongoing task</p>
+                    activeTab === 'new' && <p>new task</p>
+
+                }
+                {
+                    activeTab === 'ongoing' && <p>ongoing task</p>
+
+                }
+                {
+                    activeTab === 'completed' && <p>completed task</p>
 
                 }
             </div>
