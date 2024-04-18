a76d6949e66476969532d6992dba008b66eeafdb
Abusantos <abusomwansantos@gmail.com>
2024-04-18 15:27:25 +0100

    updated the profile component, not sure where the cprofile err coming frm

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,65 +1,65 @@
-// "use client"
-// import { useSession } from "next-auth/react"
-// import { useEffect, useState } from "react"
-// import CompletedTask from "./CompletedTask"
-// import OngoingTask from "./OngoingTask"
-// import { useSearchParams } from "next/navigation"
+"use client"
+import { useSession } from "next-auth/react"
+import { useEffect, useState } from "react"
+import CompletedTask from "./CompletedTask"
+import OngoingTask from "./OngoingTask"
+import { useSearchParams } from "next/navigation"
 
 
-// const Profile = () => {
-//     const params = useSearchParams()
-//     const childId = params.get('id')
-//     console.log(childId, "params");
+const Profile = () => {
+    const params = useSearchParams()
+    const childId = params.get('id')
+    console.log(childId, "params");
 
-//     const { data: session } = useSession()
-//     const userId = (session?.user as any)?.id
-//     const userRole = (session?.user as any)?.role
-//     let api
+    const { data: session } = useSession()
+    const userId = (session?.user as any)?.id
+    const userRole = (session?.user as any)?.role
+    let api
 
-//     if (userRole === "child") {
-//         api = `api/users/kids/${userId}/kid`
-//     } else {
-//         api = `api/users/${userId}/user`
+    const [data, setData] = useState([])
+    if (userRole === "child") {
+        api = `api/users/kids/${userId}/kid`
+    } else {
+        api = `api/users/${userId}/user`
 
-//     }
+    }
 
-//     const [data, setData] = useState()
-//     useEffect(() => {
-//         const fetchUserTask = async () => {
-//             try {
-//                 const res = await fetch(api);
-//                 const data = await res.json()
-//                 setData(data)
+    useEffect(() => {
+        const fetchUserTask = async () => {
+            try {
+                const res = await fetch(api);
+                const data = await res.json()
+                setData(data)
 
-//             } catch (error) {
-//                 console.log(error);
-//             }
-//         }
-//         fetchUserTask()
-//     }, [api])
-//     console.log("Session from user")
-//     return (
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchUserTask()
+    }, [api])
+    console.log("Session from user")
+    return (
 
-//         <div>
-//             {/* <img src={data?.image} alt="" /> */}
-//             <div className=''>
-//                 <h2>Name: {session?.user?.name}</h2>
-//                 <h4>Total Points Earned : {(data)?.points}</h4>
-//                 <div>
-//                     <div>
-//                         <h2>Completed Task</h2>
-//                         <CompletedTask />
-//                     </div>
-//                     <div>
-//                         <h2>Ongoing Task</h2>
-//                         <OngoingTask />
-//                     </div>
+        <div>
+            {/* <img src={data?.image} alt="" /> */}
+            <div className=''>
+                <h2>Name: {session?.user?.name}</h2>
+                {/* <h4>Total Points Earned : {(data)?.points}</h4> */}
+                <div>
+                    <div>
+                        <h2>Completed Task</h2>
+                        {/* <CompletedTask /> */}
+                    </div>
+                    <div>
+                        <h2>Ongoing Task</h2>
+                        {/* <OngoingTask /> */}
+                    </div>
 
-//                 </div>
+                </div>
 
-//             </div>
-//         </div>
-//     )
-// }
+            </div>
+        </div>
+    )
+}
 
-// export default Profile
+export default Profile
