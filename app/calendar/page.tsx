db522c307bbc76f2dcdef2dedbf224cdaf0f459a
Abusantos <abusomwansantos@gmail.com>
2024-05-07 15:56:16 +0100

    adding event by dragging it to the date

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,15 +1,94 @@
+"use client"
+import FullCalendar from '@fullcalendar/react'
+import interactionPlugin, { Draggable, DropArg } from "@fullcalendar/interaction" // needed for dayClick
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid' // a plugin!
+import { useState, useEffect, useRef } from 'react'
 
-import Calendar from "@/components/schedules/calendar";
-
-
-const page = () => {
-    return (
-        <div className="">
-            <div className="">
-                <Calendar />
-            </div>
-        </div>
-    )
+interface Event {
+    title: string;
+    start: Date | string;
+    allDay: boolean;
+    id: number;
 }
 
-export default page;
+const Calendar = () => {
+    const [events, setEvents] = useState([
+        { title: 'event 1', id: '1' },
+        { title: 'event 2', id: '2' },
+        { title: 'event 3', id: '3' },
+        { title: 'event 4', id: '4' },
+        { title: 'event 5', id: '5' },
+    ])
+    const draggableContainer = useRef(null)
+    const [allEvents, setAllEvents] = useState<Event[]>([])
+    const [showModal, setShowModal] = useState(false)
+    const [showDeleteModal, setShowDeleteModal] = useState(false)
+    const [idToDelete, setIdToDelete] = useState<number | null>(null)
+    const [newEvent, setNewEvent] = useState<Event>({
+        title: '',
+        start: '',
+        allDay: false,
+        id: 0
+    })
+
+    useEffect(() => {
+        let draggableEl = document.getElementById('draggable-el')
+        // console.log(draggableEl, draggableContainer.current)
+        if (draggableContainer.current) {
+            new Draggable(draggableContainer.current, {
+                itemSelector: ".fc-event",
+                eventData: function (eventEl) {
+                    let title = eventEl.getAttribute("title")
+                    let id = eventEl.getAttribute("data")
+                    let start = eventEl.getAttribute("start")
+                    return { title, id, start }
+                }
+            })
+        }
+    }
+        , [])
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between">
+            <div className=" bg-red-500  ">
+                <div className="w-full">
+                    <FullCalendar
+                        plugins={[
+                            dayGridPlugin,
+                            interactionPlugin,
+                            timeGridPlugin
+                        ]}
+                        headerToolbar={{
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'resourceTimelineWook, dayGridMonth,timeGridWeek'
+                        }}
+                        // events={allEvents as EventSourceInput}
+                        nowIndicator={true}
+                        editable={true}
+                        droppable={true}
+                        selectable={true}
+                        selectMirror={true}
+                    // dateClick={handleDateClick}
+                    // drop={(data) => addEvent(data)}
+                    // eventClick={(data) => handleDeleteModal(data)}
+                    />
+                </div>
+                <div ref={draggableContainer} id="draggable-el" className=" w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-50">
+                    <h1 className="font-bold text-lg text-center">Drag Event</h1>
+                    {events.map(event => (
+                        <div
+                            className="fc-event border-2 p-1 m-2 w-full rounded-md ml-auto text-center bg-white"
+                            title={event.title}
+                            key={event.id}
+                        >
+                            {event.title}
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </main >
+    );
+}
+
+export default Calendar;
--- components/schedules/calendar.tsx
+++ components/schedules/calendar.tsx
@@ -3,7 +3,7 @@ import FullCalendar from '@fullcalendar/react'
 import interactionPlugin, { Draggable, DropArg } from "@fullcalendar/interaction" // needed for dayClick
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid' // a plugin!
-import { useState , useEffect} from 'react'
+import { useState, useEffect } from 'react'
 
 interface Event {
     title: string;
@@ -32,23 +32,37 @@ const Calendar = () => {
     })
 
     return (
-        <FullCalendar
-            plugins={[dayGridPlugin,
-                interactionPlugin,
-                timeGridPlugin]}
-            // initialView=" timeGridWeek"
-            headerToolbar={{
-                left: "prev, next today",
-                center: "title",
-                right: "resourceTimeLineWook, dayGridMonth, timeGridWeek"
-            }}
-            events={{}}
-            nowIndicator={true}
-            editable={true}
-            droppable={true}
-            selectMirror={true}
-            selectable={true}
-        />
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="grid grid-cols-10">
+                <div className="col-span-8">
+                    <FullCalendar
+                        plugins={[
+                            dayGridPlugin,
+                            interactionPlugin,
+                            timeGridPlugin
+                        ]}
+                        headerToolbar={{
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'resourceTimelineWook, dayGridMonth,timeGridWeek'
+                        }}
+                        // events={allEvents as EventSourceInput}
+                        nowIndicator={true}
+                        editable={true}
+                        droppable={true}
+                        selectable={true}
+                        selectMirror={true}
+                    // dateClick={handleDateClick}
+                    // drop={(data) => addEvent(data)}
+                    // eventClick={(data) => handleDeleteModal(data)}
+                    />
+                </div>
+                <div id="draggable-el" className="ml-8 w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-50">
+                    <h1 className="font-bold text-lg text-center">Drag Event</h1>
+
+                </div>
+            </div>
+        </main >
     );
 }
 
