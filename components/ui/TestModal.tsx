864eba214651b11cf5d46469ba5b9ca5736081bb
Abusantos <abusomwansantos@gmail.com>
2024-05-09 23:55:45 +0100

    we created another modal using react modal

--- components/ui/TestModal.tsx
+++ components/ui/TestModal.tsx
@@ -7,16 +7,27 @@ function TestModal({ closeModal, openModal, modalIsOpen, setIsOpen }) {
     const [end, setEnd] = useState<Date>(new Date())
     const [title, setTitle] = useState<string>("")
 
+    const customStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            height: "55%",
+            width: "80%"
+        },
+    };
     return (
-        <div>
+        <div className=' h-32'>
             <Modal
                 isOpen={modalIsOpen}
                 // onAfterOpen={afterOpenModal}
                 onRequestClose={closeModal}
-            // style={customStyles}
+                style={customStyles}
             >
                 <button onClick={closeModal}>close</button>
-                <div>I am a modal</div>
                 <form>
                     <input type='text' onChange={(e) => setTitle(e.target.value)} />
 
@@ -24,13 +35,13 @@ function TestModal({ closeModal, openModal, modalIsOpen, setIsOpen }) {
                 </form>
                 <div>
                     <div>
-                        <label>
-                            Start Date
+                        <label className='text-sm font-semibold'>
+                            Select start Date
                         </label>
                         <Datetime value={start} onChange={(date) => setStart(date)} />
                     </div>
                     <div>
-                        <label>
+                        <label className='text-sm font-semibold'>
                             End Date
                         </label>
                         <Datetime value={end} onChange={(date) => setEnd(date)} />
@@ -38,22 +49,26 @@ function TestModal({ closeModal, openModal, modalIsOpen, setIsOpen }) {
                     </div>
                 </div>
 
-                <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                    <button
-                        type="submit"
-                        className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25"
-                    // disabled={newEvent.title === ''}
-                    >
-                        Create
-                    </button>
-                    <button
-                        type="button"
-                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
-                        onClick={closeModal}
-                    >
-                        Cancel
-                    </button>
+                <div >
+                    <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3
+                ">
+                        <button
+                            type="submit"
+                            className="inline-flex w-full justify-center rounded-md bg-violet-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600 sm:col-start-2 disabled:opacity-25"
+                        // disabled={newEvent.title === ''}
+                        >
+                            Create
+                        </button>
+                        <button
+                            type="button"
+                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0"
+                            onClick={closeModal}
+                        >
+                            Cancel
+                        </button>
+                    </div>
                 </div>
+
             </Modal>
         </div>
     )
