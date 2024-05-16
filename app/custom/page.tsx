ddf04ab18a08dc9e3bb7b4449f2656172d785e18
Abusantos <abusomwansantos@gmail.com>
2024-05-16 08:55:04 +0100

    Add background image to second section of page
    - Import background image () and display it in the second section of the page using the  component from .
    - Arrange the layout of the second section with a  element for the image and a  for additional content.

--- app/custom/page.tsx
+++ app/custom/page.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import bggoal from "@/public/images/bggoal.png"
+import Image from 'next/image'
 
 const page = () => {
     return (
-        <div>page</div>
+        <main>
+            <section>
+                <p>1</p>
+                <hr />
+                <p>2</p>
+                <hr />
+                <p>3</p>
+                <hr />
+            </section>
+            <section>
+                <figure>
+                    <Image src={bggoal} width={100} height={100} alt="bggoal" />
+                </figure>
+                <div>
+                    
+                </div>
+            </section>
+        </main>
     )
 }
 
