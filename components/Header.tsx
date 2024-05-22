cd8be51694193b51177341e754a01c6afeeab785
Abusantos <abusomwansantos@gmail.com>
2024-05-22 12:35:48 +0100

    restyling the parent header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,11 +5,13 @@ import Image from "next/image"
 import { useState } from "react"
 import { IoSearchOutline } from "react-icons/io5";
 import parent from "@/public/images/parent.png"
-import { Fredoka } from "next/font/google";
+import { Fredoka, Montserrat } from "next/font/google";
 import Notification from "./Notification";
 import { IoMdLogOut } from "react-icons/io";
+import Link from "next/link";
+import { FaPlus } from "react-icons/fa";
 
-
+const montserrat = Montserrat({ subsets: ["latin"] });
 const fredoka = Fredoka({ subsets: ["latin"] })
 
 const Header = () => {
@@ -24,8 +26,7 @@ const Header = () => {
                         <div className="flex items-center  space-x-2">
                             <Image src={parent} width={50} height={50} className="rounded-full" alt="parent avatar" />
                             <div className="text-gray-50 ">
-                                <h4 className="text-sm">Hi {session?.user?.name},👋🏾</h4>
-                                <h3>WELCOME</h3>
+                                <h4 className="text-lg">Hi {session?.user?.name},👋🏾</h4>
                             </div>
                         </div>
                         <div className="text-gray-50 flex judstify-center items-center space-x-2">
@@ -38,10 +39,18 @@ const Header = () => {
                     </div>
 
                 </div>
-                <div className="flex items-center bg-gray-50  w-full p-2 rounded-md space-x-2">
+                <div className=" bal-image relative flex flex-col items-center justify-center">
+                    <h3 className={`${fredoka.className} text-gray-300 text-center`}> Balance</h3>
+                    <h1 className="text-3xl text-gray-100">⭐️ 30,000,000</h1>
+                    <Link href="/createTask" className="absolute -bottom-2 text-gray-100 text-xl ">
+                        <FaPlus />
+                    </Link>
+                </div>
+
+                {/* <div className="flex items-center bg-gray-50 w-full p-2 rounded-md space-x-2">
                     <IoSearchOutline />
                     <input type="text" placeholder="Search for task" className="outline-none p2" />
-                </div>
+                </div> */}
             </div>
         </div>
     )
