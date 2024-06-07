057518ba08a2d128a3b3923757a1759850e844cd
Abusantos <abusomwansantos@gmail.com>
2024-06-07 01:12:31 +0100

    designed messages for parent and child

--- components/tasks/CommentReply.tsx
+++ components/tasks/CommentReply.tsx
@@ -1,11 +1,16 @@
 "use client"
 import useSocket from '@/context/useSocket';
 import { FormatTimeDifference } from '@/lib/FormatTime';
+import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react'
 
 const CommentReply = ({ taskId }) => {
     const [messages, setMessages] = useState([])
+    const { data: session } = useSession()
+
+    const userId = (session?.user as any)?.id
+
     useEffect(() => {
         if (taskId) {
             const fetchComment = async () => {
@@ -17,7 +22,7 @@ const CommentReply = ({ taskId }) => {
                     }
                     const data = await res.json()
                     setMessages(data)
-                    console.log(data)
+                    // console.log(data)
                 } catch (error) {
                     console.log(error.message)
                 }
@@ -29,23 +34,49 @@ const CommentReply = ({ taskId }) => {
     return (
         <div>
             {
-                messages.map((message, index) => {
-                    console.log(message)
-                    // console.log(message["childId"].image)
+                messages.map((dm, index) => {
+                    // console.log(dm)
+                    const { _id, message, createdAt, creator } = dm
+                    const childName = dm["childId"].username
+                    const parentName = dm["parentId"].username
+
+                    let isCurrentUserMessage: boolean
+                    if (creator === userId) {
+                        isCurrentUserMessage = true
+                    }
+
                     return (
-                        <div key={index} className=' py-2'>
-                            <div className='flex items-center flex-start space-x-2 text-gray-900'>
-                                <Image src={message["childId"].image} alt={message["childId"].username} width={30} height={30} />
-                                <div className='flex flex-col '>
-                                    <span className='text-[0.7rem] capitalize'>{message["childId"].username}</span>
-                                    <span className='text-[0.45rem] text-gray-500'>
-                                        {FormatTimeDifference(message.createdAt)}
-                                    </span>
+                        <div key={index} >
+                            <div className={`flex  ${isCurrentUserMessage ? "justify-end" : "justify-start"} `}>
+
+                                <div>
+                                    <div className='flex items-center flex-start space-x-2 text-gray-900'>
+                                        <Image src={dm["childId"].image} alt={dm["childId"].username} width={30} height={30} />
+                                        <div className='flex flex-col '>
+                                            {
+                                                creator === userId ?
+                                                    <span className='text-[0.7rem] capitalize'>
+                                                        {
+                                                            childName
+                                                        }
+                                                    </span> :
+                                                    <span className='text-[0.7rem] capitalize'>
+                                                        {
+                                                            parentName
+                                                        }
+                                                    </span>
+                                            }
+
+                                            <span className='text-[0.45rem] text-gray-500'>
+                                                {FormatTimeDifference(dm.createdAt)}
+                                            </span>
+                                        </div>
+                                    </div>
+
+                                    <p className='text-gray-800 text-[0.7rem] px-9 '>{dm.message}</p>
                                 </div>
                             </div>
 
-
-                            <p className='text-gray-800 text-[0.7rem] px-9 '>{message.message}</p>
                         </div>
                     )
                 }, [])
